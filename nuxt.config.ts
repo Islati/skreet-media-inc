2cd7ed2ba5afce615f6ca521611e6f3135376605
Islati <islati.sk@gmail.com>
2024-05-27 21:19:11 -0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: {enabled: true},
+    ssr: true,
+    css: [`assets/css/styles.css`],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        'shadcn-nuxt',
+        "@nuxtjs/sitemap",
+        "@nuxtjs/seo",
+        '@nuxtjs/robots',
+        "@nuxt/content",
+        '@nuxtjs/color-mode',
+        "@nuxt/image"
+    ],
+    robots: {},
+    seo: {
+        redirectToCanonicalSiteUrl: true
+    },
+    site: {
+        url: 'https://skreet.ca',
+        title: 'Skreet',
+        description: 'Skreet Media Inc. Record Label Official Website',
+    },
+    sitemap: {
+        cacheMaxAgeSeconds: 3600, // 1 hour
+        discoverImages: true,
+        routes: ['/'],
+    },
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    },
+    nitro: {
+        prerender: {
+            routes: ['/sitemap.xml']
+        }
+    },
+    content: {}
+})
